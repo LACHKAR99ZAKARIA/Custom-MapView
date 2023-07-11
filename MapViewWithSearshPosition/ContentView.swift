70f549190a9109a633968deb86d511dc07ad50de
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-07-11 09:33:28 +0100

    Initial Commit

---
+++ MapViewWithSearshPosition/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapViewWithSearshPosition
+//
+//  Created by Zakarai Lachkar on 11/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapViewWithSearshPosition/MapViewWithSearshPositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewWithSearshPositionApp.swift
+//  MapViewWithSearshPosition
+//
+//  Created by Zakarai Lachkar on 11/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapViewWithSearshPositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
