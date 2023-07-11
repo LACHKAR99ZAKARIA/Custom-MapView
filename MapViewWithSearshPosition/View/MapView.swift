ecf0c8294ba021c304d365a25477dfd64978919c
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-07-11 10:01:04 +0100

    setup project

--- MapViewWithSearshPosition/View/MapView.swift
+++ MapViewWithSearshPosition/View/MapView.swift
@@ -24,18 +24,38 @@ struct MapView: UIViewRepresentable {
     func updateUIView(_ uiView: MKMapView, context: Context) {
 
     }
+    class CustomAnnotationView: MKAnnotationView {
+        override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+            super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            self.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+            self.addSubview(Circle()
+                .frame(width: 10, height: 10)
+                .foregroundColor(.red) as! UIView
+            )
+            self.canShowCallout = true
+            self.addDropAnimation()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        private func addDropAnimation() {
+            let animation = CAKeyframeAnimation(keyPath: "transform.translation.y")
+            animation.values = [0, 10, -10, 5, -5, 0]
+            animation.keyTimes = [0, 0.25, 0.5, 0.75, 0.9, 1]
+            animation.duration = 0.5
+            self.layer.add(animation, forKey: "dropAnimation")
+        }
+    }
 
     class Coordinator: NSObject, MKMapViewDelegate {
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            // Custom pins
-            // Excluding user blue circle
-            if annotation.isKind(of: MKUserLocation.self) { return nil }
-            else {
-                let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "PIN_VIEW")
-                pinAnnotation.tintColor = .red
-                pinAnnotation.animatesDrop = true
-                pinAnnotation.canShowCallout = true
-                return pinAnnotation
+            if annotation is MKUserLocation {
+                return nil
+            } else {
+                let annotationView = CustomAnnotationView(annotation: annotation, reuseIdentifier: "customAnnotation")
+                return annotationView
             }
         }
     }
